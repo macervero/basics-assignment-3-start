df6364f0dd542a4d1d955a70b7986142fc17a775
Maria dels Àngels Cerveró Abelló <mangels16@gmail.com>
2024-02-03 20:07:05 +0100

    Third assignment finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public display: boolean = false;
+  public timestampClicks: string[] = [];
+
+  public displayDetails(): void {
+    let date: Date = new Date();
+
+    this.display = true;
+    this.timestampClicks.push(date.toTimeString());
+  }
 }
